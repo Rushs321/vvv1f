5820a4a0c36bce039d9cc4c1ea93cf5350ed0b56
Rushs321 <fyggh566@tutamail.com>
2024-08-29 10:57:18 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,20 +1,17 @@
+"use strict";
 const DEFAULT_QUALITY = 40;
 
 async function params(request, reply) {
-  const { url, jpeg, bw, l } = request.query;
-
+  let url = request.query.url;
   if (!url) {
     return reply.send('bandwidth-hero-proxy');
   }
 
-  const urls = Array.isArray(url) ? url.join('&url=') : url;
-  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
   request.params = {
-    url: cleanedUrl,
-    webp: !jpeg,
-    grayscale: bw !== '0',
-    quality: parseInt(l, 10) || DEFAULT_QUALITY
+    url: decodeURIComponent(url),
+    webp: !request.query.jpeg,
+    grayscale: request.query.bw != 0,
+    quality: parseInt(request.query.l, 10) || DEFAULT_QUALITY
   };
 }
 
